9fffe8056ec38c18e3e8db6380584b867503ff52
tayzar24 <41561653+tayzar24@users.noreply.github.com>
2025-03-04 14:58:43 +0630

    Update UnitTestOfGenerateText.cs

--- CodingTest-main/OldPhonePad.UnitTest/UnitTestOfGenerateText.cs
+++ CodingTest-main/OldPhonePad.UnitTest/UnitTestOfGenerateText.cs
@@ -5,27 +5,42 @@ namespace OldPhonePad.UnitTest
     public class UnitTestOfGenerateText
     {
         [Test]
-        public  void Test_SingleKeyPress()
-        {
-            GenerateTextHelper.Initialize();
+public  void Test_SingleKey()
+{
+    GenerateTextHelper.Initialize();
 
-            Assert.AreEqual("E", GenerateTextHelper.Generate("33#"));
-        }
+    Assert.AreEqual("E", GenerateTextHelper.Generate("33#"));
+}
 
-        [Test]
-        public void Test_MultipleKeys_Pauses()
-        {
-            GenerateTextHelper.Initialize();
-            Assert.AreEqual("HELLO", GenerateTextHelper.Generate("4433555 555666#"));
-        }
+[Test]
+public void Test_Backspace()
+{
+    GenerateTextHelper.Initialize();
+    Assert.AreEqual("B", GenerateTextHelper.Generate("227*#"));
+}
 
-        [Test]
-        public void Test_WithBackspace()
-        {
-            GenerateTextHelper.Initialize();
-            string result = GenerateTextHelper.Generate("8 2 999 9999 2 777 6 999 2 8 66 666 33#");
-            Assert.AreEqual("TAYZARMYATNOE", result);
-        }
+[Test]
+public void Test_MultipleKeysAndWhiteSpace()
+{
+    GenerateTextHelper.Initialize();
+    Assert.AreEqual("HELLO", GenerateTextHelper.Generate("4433555 555666#"));
+}
+
+[Test]
+public void Test_WithDuplicateHash()
+{
+    GenerateTextHelper.Initialize();
+    string result = GenerateTextHelper.Generate("33##");
+    Assert.AreEqual("E", result);
+}
+
+[Test]
+public void Test_WithNotEnterHashKey()
+{
+    GenerateTextHelper.Initialize();
+    string result = GenerateTextHelper.Generate("33");
+    Assert.AreEqual("33", result);
+}
 
     }
 }
